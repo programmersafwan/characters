487aacc280f82f721d9c8c4f5c689c268de8affb
programmersafwan <safwanprogramming@gmail.com>
2020-02-17 16:30:39 +0800

    first commit

---
+++ build/precache-manifest.4d301ac4295475f6d93181cc87fd948e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aad497013590ffe447cfce3e38768a0",
+    "url": "/characters/index.html"
+  },
+  {
+    "revision": "ec102d7ee2361ee2502e",
+    "url": "/characters/static/css/main.c57d0626.chunk.css"
+  },
+  {
+    "revision": "3aaa761c2e5f24cadb4b",
+    "url": "/characters/static/js/2.54257792.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/characters/static/js/2.54257792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec102d7ee2361ee2502e",
+    "url": "/characters/static/js/main.85edf99c.chunk.js"
+  },
+  {
+    "revision": "5e4466355c9a64c7a75a",
+    "url": "/characters/static/js/runtime-main.1fa7637a.js"
+  }
+]);
